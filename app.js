ac60416cf12f6cc1d570a66367c3868f91652871
sahay-saurabh <saurabhj12312312@gmail.com>
2021-12-02 03:01:08 +0530

    minor change

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ app.set('view engine','ejs')
 app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
-    res.render('index',{data:data});
+    res.render('index',{data:data,head:"Home"});
 })
 
 app.get('/:posts',function(req,res){
     for(var i=0;i<data.length;i++){
         if(data[i].mentor==req.params.posts){
-            res.render('post',{content:data[i]});
+            res.render('post',{content:data[i],head:"Projects"});
         }
         if(i==data.length){
             res.redirect('/');
